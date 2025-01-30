f8be6250d38c2f12ac57113a88ea2095576e56fe
jongseok-lee <jongseok.lee@linecorp.com>
2025-01-30 10:47:34 +0900

    nx: setting up the monorepo with react and rspack

---
+++ apps/products/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+import '../styles.css';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="products" />
+    </div>
+  );
+}
+
+export default App;
