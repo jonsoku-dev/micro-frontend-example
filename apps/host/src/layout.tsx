aab791c15837b63fe94e016a2cbe76380d256ed0
Ricky <jonsoku.dev@gmail.com>
2025-01-30 20:44:33 +0900

    section3: React Router - Adding nested routing

--- apps/host/src/layout.tsx
+++ apps/host/src/layout.tsx
@@ -1,21 +1,25 @@
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect } from 'react';
 import { logger } from '@custom-mfe/logger';
-import User from "./app/user";
-import { useQuery } from "@tanstack/react-query";
+import User from './app/user';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Layout({ children }: PropsWithChildren) {
-    useEffect(() => {
-        return logger.subscribe((event) => {
+  useEffect(() => {
+    return logger.subscribe((event) => {});
+  }, []);
 
-        });
-    }, [])
-
-    return (
-        <>
-            <nav style={{ background: 'rgb(36, 36, 36)', padding: '1rem', color: '#fff' }}>
-                <User />
-            </nav>
-            {children}
-        </>
-    )
+  return (
+    <>
+      <nav
+        style={{
+          background: 'rgb(36, 36, 36)',
+          padding: '1rem',
+          color: '#fff',
+        }}
+      >
+        <User />
+      </nav>
+      {children}
+    </>
+  );
 }
--- apps/host/src/root.tsx
+++ apps/host/src/root.tsx
@@ -10,28 +10,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import User from './app/user';
 
 init({
-    name: 'host',
-    remotes: []
+  name: 'host',
+  remotes: [],
 });
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
-    <StrictMode>
-        <ProfileProvider>
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <Layout>
-                        <Routes>
-                            <Route path="/*" element={<App />} />
-                            <Route path="/user" element={<User />} />
-                        </Routes>
-                    </Layout>
-                </Router>
-            </QueryClientProvider>
-        </ProfileProvider>
-    </StrictMode>
+  <StrictMode>
+    <ProfileProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/*" element={<App />} />
+              <Route path="/user" element={<User />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </QueryClientProvider>
+    </ProfileProvider>
+  </StrictMode>
 );
